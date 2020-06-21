8755a6e6c6b9e0c8c07576f739a146d7515ddcaa
gelvezz23 <gelvezz223@gmail.com>
2020-06-21 04:11:47 -0500

    variables and constansts

---
+++ 02-introduccion a javascript moderno/src/Bases/01-const-let.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const nombre = 'carlos';
+let apellido = 'gomez';
+let valorDado = 6;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+	const nombre = 'mario';
+	let valorDado = 5;
+	console.log(nombre, valorDado);
+}
+
+console.log(valorDado);
