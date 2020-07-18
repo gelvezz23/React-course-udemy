fd381057836c4be7a90ff2ca350574d567a056cc
gelvezz23 <gelvezz223@gmail.com>
2020-07-18 03:18:36 -0500

    add css and create first componnet

---
+++ 03-primeros-pasos-en-react/counter-app/src/PrimeraApp.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const PrimeraApp = () => {
+	return <h1>Hola mundo</h1>;
+};
+
+export default PrimeraApp;
--- 03-primeros-pasos-en-react/counter-app/src/index.js
+++ 03-primeros-pasos-en-react/counter-app/src/index.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-const saludar = <h1>Hola mundo</h1>;
-
 // reactDOM tiene un metodo llamado render
 // este metodo recibe dos parametros ( lo que voy a hacer, y donde lo quiero hacer)
-ReactDOM.render(saludar, divRoot);
+ReactDOM.render(<PrimeraApp />, divRoot);
