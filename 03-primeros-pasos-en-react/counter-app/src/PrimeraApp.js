4baf2680f5f23a8e6b9acc1e295dc306f478a766
gelvezz23 <gelvezz223@gmail.com>
2020-07-18 04:17:51 -0500

    default props

--- 03-primeros-pasos-en-react/counter-app/src/PrimeraApp.js
+++ 03-primeros-pasos-en-react/counter-app/src/PrimeraApp.js
@@ -3,19 +3,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 	return (
 		// este es un fragment abreviado
 		<>
 			{/* se puede hacer asi pero no se usa <h1>{props.saludo}</h1> */}
 			<h1>{saludo}</h1>
-			<p>esto es una props con destructuracion</p>
+			<p>{subtitulo}</p>
 		</>
 	);
 };
 
+// obligo a que los props tengan valores y envien datos con tipos de datos
+
 PrimeraApp.propTypes = {
 	saludo: PropTypes.string.isRequired,
+};
+
+// que mis props tengan valores por defecto
+PrimeraApp.defaultProps = {
+	subtitulo: 'Soy un subtitulo por defecto',
 };
 
 export default PrimeraApp;
