b3d21af7ee08b89e12053347e699e96b844a06e1
gelvezz23 <gelvezz223@gmail.com>
2020-07-18 04:05:58 -0500

    clean my component

--- 03-primeros-pasos-en-react/counter-app/src/PrimeraApp.js
+++ 03-primeros-pasos-en-react/counter-app/src/PrimeraApp.js
@@ -1,57 +1,14 @@
 /** @format */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 
-const PrimeraApp = ({ saludo = 'dato por defecto' }) => {
-	// para imprimirt variables
-
-	console.log(saludo);
-
-	const saludar = 'hola mundo con una variable ';
-	const variable = 123;
-	const variable2 = 123.123;
-	const variable3 = true;
-	const variable4 = [1, 2, 3, 4, 5, 6, 7];
-	const variable5 = { nombre: 'Carlos', edad: 23 };
-
+const PrimeraApp = ({ saludo }) => {
 	return (
 		// este es un fragment abreviado
 		<>
 			{/* se puede hacer asi pero no se usa <h1>{props.saludo}</h1> */}
 			<h1>{saludo}</h1>
 			<p>esto es una props con destructuracion</p>
-			<h1>{saludar}</h1>
-			<p>Un saludo con variables</p>
-			<h1>{variable}</h1>
-			<p>imprimir numeros</p>
-
-			<h1>{variable2}</h1>
-			<p>imprimir decimales</p>
-
-			{/* Los booleanos no los imprime */}
-			<h1>{variable3}</h1>
-			<p>imprimir booleanos (no se ve)</p>
-
-			{/* Al imprimir un array los ordena y los concatena */}
-			<h1>{variable4}</h1>
-			<p> imprimir un array </p>
-
-			{/* Si queremos imprimir un objeto debemos parsarlo a json y luego 
-                imprimirlo
-            */}
-			<pre>{JSON.stringify(variable5, null, 3)}</pre>
-			<p>imprimir un objeto</p>
-
-			{/* Esto esta bien pero el div es informacion que no necesitamos */}
-			<div>
-				<h1>Hola mundo con div</h1>
-				<p> mi primera aplicacion</p>
-			</div>
-			{/* para corregir esto usamos el Fragment <></> */}
-			<Fragment>
-				<h1>Hola mundo con fragment</h1>
-				<p> mi primera aplicacion</p>
-			</Fragment>
 		</>
 	);
 };
